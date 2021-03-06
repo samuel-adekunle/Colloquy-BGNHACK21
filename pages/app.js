bbae5cf38899f0e1047058c7c62a5242bc9306d9
Tofunmi Adigun-Hameed <tofunmi.ah@gmail.com>
2021-03-06 11:33:02 +0000

    add and remove q card buttons work

--- pages/app.js
+++ pages/app.js
@@ -1,10 +1,39 @@
+import { useEffect, useState } from "react";
 import { FaPlus, FaMinus } from "react-icons/fa";
 import BotMenu from "../components/botMenu";
 import HeaderBar from "../components/headerBar";
 import QuestionCard from "../components/questionCard";
 
 export default function App() {
+  const [questionGroups, setQuestionGroups] = useState([]);
   const dummyFunction = () => { };
+
+  const removeQuestion = (id) => {
+    setQuestionGroups((oldGroups) => {
+      return (oldGroups[id].questions.pop())
+    })
+  }
+
+  const addQuestion = (id) => {
+    setQuestionGroups((oldGroups) => {
+      return (oldGroups[id].questions=[oldGroups])
+    })
+  }
+
+  const addGroup = () => {
+    setQuestionGroups([...questionGroups,{
+      title: "",
+      questions: [],
+      answer: "",
+    },]);
+  }
+  
+  const removeGroup = () => {
+    setQuestionGroups((oldGroups) => {
+      return oldGroups.splice(-1, 1);
+
+    })
+  };
   return (
     <div className="text-gray-900">
       <HeaderBar/>
@@ -17,10 +46,18 @@ export default function App() {
             title="Hello Questions"
             questions={["how are you", "how is your wife"]}
             answer="Great, thanks!"
-            />
+          />
+          {questionGroups.map((group, index) => (
+            <QuestionCard title={group.title} questions={group.questions} answer={group.answer} key={index}/>
+          ))}
           <span className="flex justify-around">
-            <button className="w-full bg-green-100 grid grid-cols-1 justify-items-center py-1 rounded focus:ring-green-200 focus:ring-opacity-50 focus:outline-none focus:ring focus:ring-offset-2 hover:bg-green-200" onClick={dummyFunction()}><i><FaPlus size={42}/></i></button>
-            <button className="w-full bg-red-100 grid grid-cols-1 justify-items-center py-1 rounded focus:ring-red-200 focus:ring-opacity-50 focus:outline-none focus:ring focus:ring-offset-2 hover:bg-red-200" onClick={dummyFunction()}><i><FaMinus size={42}/></i></button>
+            <button className="w-full bg-green-100 grid grid-cols-1 justify-items-center py-1 rounded 
+            focus:ring-green-200 focus:ring-opacity-50 focus:outline-none focus:ring focus:ring-offset-2
+             hover:bg-green-200"
+              onClick={addGroup}><i><FaPlus size={42} /></i></button>
+            <button className="w-full bg-red-100 grid grid-cols-1 justify-items-center py-1 rounded 
+            focus:ring-red-200 focus:ring-opacity-50 focus:outline-none
+            focus:ring focus:ring-offset-2 hover:bg-red-200" onClick={removeGroup}><i><FaMinus size={42} /></i></button>
             </span>
         </div>
       </div>
