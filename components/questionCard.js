79759a0046380b57d5c004378cfdc7bc3a1bf121
Tofunmi Adigun-Hameed <tofunmi.ah@gmail.com>
2021-03-06 09:31:48 +0000

    added menu and topbar components

--- components/questionCard.js
+++ components/questionCard.js
@@ -3,7 +3,7 @@ export default function QuestionCard({ title, questions, answer }) {
   const dummyFunction = () => { };
   return (
     <div className="border-4 rounded-md p-3 bg-blue-50">
-      <input className="text-2xl" value={ title} name="title" type="text"/>
+      <input className="text-2xl pl-2" value={ title} name="title" type="text"/>
       <div className="space-y-3 px-3">
         {
           questions.map((q) => (
@@ -12,14 +12,14 @@ export default function QuestionCard({ title, questions, answer }) {
             name="question"
             value={q}
               key={q}
-              className="block border-2 rounded-lg"
+              className="block border-2 rounded-lg pl-2"
               />))
             }
         <input
           type="text"
           name="question"
           placeholder="add a question here"
-          className="w-full border-2 rounded-lg"
+          className="w-full border-2 rounded-lg pl-2"
         />
         <span className="flex">
           <span className="w-full"></span>
@@ -31,7 +31,7 @@ export default function QuestionCard({ title, questions, answer }) {
           name="answer"
           placeholder="add the answer to these questions here"
           value={answer}
-          className="w-full border-b-2 rounded-lg"
+          className="w-full border-b-2 rounded-lg pl-2"
           />
       </div>
       {/* add-on changes  */}
--- pages/app.js
+++ pages/app.js
@@ -1,20 +1,29 @@
 import { FaPlus, FaMinus } from "react-icons/fa";
+import BotMenu from "../components/botMenu";
+import HeaderBar from "../components/headerBar";
 import QuestionCard from "../components/questionCard";
 
 export default function App() {
   const dummyFunction = () => { };
   return (
     <div className="text-gray-900">
-      hee
-      <QuestionCard
-        title="Hello Questions"
-        questions={["how are you", "how is your wife"]}
-        answer="Great, thanks!"
-      />
-      <span className="flex justify-around">
-        <button className="w-full bg-green-100 grid grid-cols-1 justify-items-center py-1 rounded focus:ring-green-200 focus:ring-opacity-50 focus:outline-none focus:ring focus:ring-offset-2 hover:bg-green-200" onClick={dummyFunction()}><i><FaPlus size={42}/></i></button>
-        <button className="w-full bg-red-100 grid grid-cols-1 justify-items-center py-1 rounded focus:ring-red-200 focus:ring-opacity-50 focus:outline-none focus:ring focus:ring-offset-2 hover:bg-red-200" onClick={dummyFunction()}><i><FaMinus size={42}/></i></button>
-        </span>
+      <HeaderBar/>
+      <div className="grid grid-cols-5">
+        <div>
+          <BotMenu />
+        </div>
+        <div className="col-span-4 m-7">
+          <QuestionCard
+            title="Hello Questions"
+            questions={["how are you", "how is your wife"]}
+            answer="Great, thanks!"
+            />
+          <span className="flex justify-around">
+            <button className="w-full bg-green-100 grid grid-cols-1 justify-items-center py-1 rounded focus:ring-green-200 focus:ring-opacity-50 focus:outline-none focus:ring focus:ring-offset-2 hover:bg-green-200" onClick={dummyFunction()}><i><FaPlus size={42}/></i></button>
+            <button className="w-full bg-red-100 grid grid-cols-1 justify-items-center py-1 rounded focus:ring-red-200 focus:ring-opacity-50 focus:outline-none focus:ring focus:ring-offset-2 hover:bg-red-200" onClick={dummyFunction()}><i><FaMinus size={42}/></i></button>
+            </span>
+        </div>
+      </div>
     </div>
     
   )
