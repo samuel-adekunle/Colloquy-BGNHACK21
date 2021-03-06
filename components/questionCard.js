dd11cd7efaf4c13a3053480d81eb4d33e82f1421
Tofunmi Adigun-Hameed <tofunmi.ah@gmail.com>
2021-03-06 00:36:37 +0000

    question card styled and done

--- components/questionCard.js
+++ components/questionCard.js
@@ -1,9 +1,10 @@
-export default function QuestionCard({ title, questions, answer}) {
+import { FaPlus, FaMinus } from "react-icons/fa";
+export default function QuestionCard({ title, questions, answer }) {
   const dummyFunction = () => { };
   return (
-    <div className="border-2 rounded-md">
-      <h2 className="text-2xl">{title}</h2>
-      <div className="">
+    <div className="border-4 rounded-md p-3 bg-blue-50">
+      <input className="text-2xl" value={ title} name="title" type="text"/>
+      <div className="space-y-3 px-3">
         {
           questions.map((q) => (
             <input
@@ -11,25 +12,26 @@ export default function QuestionCard({ title, questions, answer}) {
             name="question"
             value={q}
               key={q}
-              className=""
+              className="block border-2 rounded-lg"
               />))
             }
         <input
           type="text"
           name="question"
           placeholder="add a question here"
-          className="w-full"
+          className="w-full border-2 rounded-lg"
         />
-        <span>
-          <button onClick={dummyFunction()}>plus</button>
-          <button onClick={dummyFunction()}>minus</button>
+        <span className="flex">
+          <span className="w-full"></span>
+          <button className="mx-2" onClick={dummyFunction()}><FaPlus /></button>
+          <button className="mx-2 hover:bg-red-200" onClick={dummyFunction()}><FaMinus /></button>
         </span>
          <input
           type="text"
           name="answer"
           placeholder="add the answer to these questions here"
           value={answer}
-          className="w-full"
+          className="w-full border-b-2 rounded-lg"
           />
       </div>
       {/* add-on changes  */}
