eeaaa9d995f7b24a63d0084e7d9930feb5857c13
Samuel Adekunle <me@samuel-adekunle.software>
2021-03-06 12:32:11 +0000

    order
    Signed-off-by: Samuel Adekunle <me@samuel-adekunle.software>

--- components/questionCard.js
+++ components/questionCard.js
@@ -1,4 +1,4 @@
-import { FaPlus, FaMinus } from "react-icons/fa";
+import { FaMinus, FaPlus } from "react-icons/fa";
 export default function QuestionCard({ title, questions, answer }) {
   const dummyFunction = () => { };
   return (
