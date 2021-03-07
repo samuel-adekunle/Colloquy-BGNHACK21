6fa8451c5f1e4dc7037cdb522b59dc8d0c03fd9d
Dileep <dkbollaram2020@gmail.com>
2021-03-07 11:20:03 +0000

    chat-uiv2 updated changes
    simple working chatbot integrated with backend api

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,3 @@
-import ChatWidget from "../components/ChatWidget";
-
 export default function Index() {
-  function didReceiveNewUserMessage(message) {
-    console.log(`New message incoming! ${message}`);
-
-    // Now send the message throught the backend API
-  }
-
-  return (
-    <div>
-      {true && <ChatWidget handleNewUserMessage={didReceiveNewUserMessage} />}
-    </div>
-  );
+  return <div>index page</div>;
 }
