706eaa4891d25bb4e15d4c41bef4f0739d463e96
Samuel Adekunle <me@samuel-adekunle.software>
2021-03-07 11:47:49 +0000

    buttonFooter
    Signed-off-by: Samuel Adekunle <me@samuel-adekunle.software>

--- components/buttonFooter.js
+++ components/buttonFooter.js
@@ -1,49 +1,47 @@
 const intentsTest = [
-  {
-    tag: "hello",
-    patterns: ["hi", "how are you"],
-    responses: ["hey"],
-  },
-  {
-    tag: "test",
-    patterns: ["jkl", "asdf"],
-    responses: ["1234"],
-  },
+	{
+		tag: "hello",
+		patterns: ["hi", "how are you"],
+		responses: ["hey"],
+	},
+	{
+		tag: "test",
+		patterns: ["jkl", "asdf"],
+		responses: ["1234"],
+	},
 ];
 export default function ButtonFooter({
-  removeBot,
-  canDelete,
-  addUrlGeneratedQuestions,
+	removeBot,
+	canDelete,
+	addUrlGeneratedQuestions,
+	trainBot
 }) {
-  const generateQuestions = () => {
-    let url = prompt("Enter the website url:");
-    //alert(url)
-    // some code to use the url to get the intents
-    addUrlGeneratedQuestions(intentsTest);
-  };
-  const phantomStyle = {
-    display: "block",
-    padding: "20px",
-    height: "60px",
-    width: "100%"
-  };
-  return (
-    <div>
-      <div className="phantom" />
-      <header className="bg-gray-50 text-center p-3 bottom-0 w-full flex justify-evenly fixed">
-        <button className="btn" onClick={generateQuestions}>
-          Generate Questions &amp; Answers from Website
+	const generateQuestions = () => {
+		let url = prompt("Enter the website url:");
+		//alert(url)
+		// some code to use the url to get the intents
+		addUrlGeneratedQuestions(intentsTest);
+	};
+	const phantomStyle = {
+		display: "block",
+		padding: "20px",
+		height: "60px",
+		width: "100%"
+	};
+	return (
+		<div>
+			<div className="phantom" />
+			<div className="bg-gray-50 text-center p-3 bottom-0 w-4/5 flex justify-evenly fixed">
+				<button className="btn" onClick={generateQuestions}>
+					Generate Questions &amp; Answers from Website
         </button>
-        <button className="btn">Train Bot</button>
-        <button className="btn">Test Bot</button>
-        <button className="btn">Generate Embed</button>
-
-        {canDelete && (
-          <button className="btn bg-red-300 hover:bg-red-200" onClick={removeBot}>
-            Delete Bot
-          </button>
-        )}
-      </header>
-    </div>
-  );
+				<button className="btn" onClick={trainBot}>Train Bot</button>
+				<button className="btn">Test Bot</button>
+				<button className="btn">Generate Embed</button>
+				{canDelete && <button className="btn bg-red-300 hover:bg-red-200" onClick={removeBot}>
+					Delete Bot
+        </button>}
+			</div>
+		</div>
+	);
 }
